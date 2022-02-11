236b3caca85b790e3ea617b2a913bc9e58793050
Veronica <79570212+Ni4ka1991@users.noreply.github.com>
2022-02-11 13:10:30 +0200

    Create hres_1.33_2.c

---
+++ hres_1.33_2.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+#define CLS printf("\033c\n")
+#define EPS 0.00001
+
+int main () {
+	int i;
+	double qr, n = 1, k, x, y, lmt = 100;
+	CLS;
+	printf ("Введите, пожалуйста, число, \nквадратный корень ИЗ которого Вы хотите узнать (меньше числа %lg)\n", lmt*lmt);	
+	scanf("%lg", &qr);
+ 	if (qr > lmt*lmt) {
+ 		printf("Введите число меньше %lg\n", lmt*lmt);
+ 		}
+ 		else {
+		 	for (i = 0; i < lmt; i++) {
+				k = n;	
+				n++;		
+				k *= k;
+				if (qr == k) {
+					printf("Квадратный корень из числа %lg = %lg\n", qr, (n - 1));
+					break;		
+					}
+					else {
+						if (qr < k)	{
+							x = (n - 1);			
+							for (i = 0; i < 8; i++) {
+								y = x*x - qr;
+								x = (2*x*x - y)/(2*x);
+								if (y < EPS) {
+									printf("Квадратный корень из числа %lg \033[33;1m = %15.15lf\033[0m\n", qr, x);
+									y = x*x;
+									printf("%15.15lf ^ 2 = %30.30lf\n", x, y);
+									break;						
+									}					
+								} 			
+							break;
+							}			
+						}
+					}
+			}
+}
