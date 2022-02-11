4c1d6a633d239bc295c1b1fe5cfc5a523ba62450
Veronica <79570212+Ni4ka1991@users.noreply.github.com>
2022-02-11 12:32:52 +0200

    Create 2.13b.c

---
+++ 2.13b.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+int a, b, m, n, z;
+m = n = 5;
+z = a = b = 0;
+z--,
+b = z + (m != n);
+printf("\033c a = %d, b = %d, m = %d, n = %d, z = %d\n", a, b, m, n, z);
+
+
+	}
