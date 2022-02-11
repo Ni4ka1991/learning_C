dd1ebf4067186bd52b15d204c63a2cf0470aa224
Veronica <79570212+Ni4ka1991@users.noreply.github.com>
2022-02-11 12:52:34 +0200

    Create color_str.c

---
+++ color_str.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <math.h>
+
+#include <stdio.h>
+int main() {
+	int i;
+	printf("\033[30mЧерный \033[0m\n");
+	printf("\033[31mКрасный \033[0m\n");
+	printf("\033[32mЗеленый \033[0m\n");
+	printf("\033[33mКоричневый \033[0m\n");
+	printf("\033[34mСиний \033[0m\n");
+	printf("\033[35mФиолетовый \033[0m\n");
+	printf("\033[36mГолубой \033[0m\n");
+	printf("\033[37mСерый \033[0m\n");
+	printf("\033[37;1mБелый \033[0m\n");
+	printf("\033[33;1mЖелтый \033[0m\n");
+	return	0;
+}
