dd064c7060c972a4696e00ec7f32dc18543345d5
Veronica <79570212+Ni4ka1991@users.noreply.github.com>
2022-02-11 12:05:31 +0200

    Create 2.12.c

---
+++ 2.12.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+char c;
+float f;
+short s;
+double d;
+int i;
+long lng;
+unsigned u;
+unsigned short us;
+signed char sc;
+long double ld;
+
+i = 1000 * 2000000; 
+printf("\033cСогласно с правилами преобразования типов в выражениях, получим: %i\n", i);
+
+
+	}
