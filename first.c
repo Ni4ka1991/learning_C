257d7ec4f0d506c715dc8665c4482fbc83756889
Veronica <79570212+Ni4ka1991@users.noreply.github.com>
2022-02-11 13:00:27 +0200

    Create first.c

---
+++ first.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+unsigned char *tags[] = {"!DOCTYPE HTML", "html", "body", "h3"};
+unsigned char marks[] = {'\"','<','/', '>'};
+
+int main() {
+int i = 0;
+
+for(i = 0; i < 3; i++){
+	printf("%c", marks[1]);
+	printf("%s", tags[i]);
+	printf("%c\n", marks[3]);
+}
+
+	printf("%c", marks[1]);
+	printf("%s", tags[3]);
+	printf("%c\n", marks[3]);
+	printf("My first CGI! It's work!");
+	printf("%c", marks[1]);
+	printf("%c\n", marks[2]);
+	printf("%s", tags[3]);
+	printf("%c\n", marks[3]);
+
+
+for(i = 1; i < 3; i++){
+	printf("%c", marks[1]);
+	printf("%c", marks[2]);	
+	printf("%s", tags[i]);
+	printf("%c\n", marks[3]);
+}
+
+
+}
