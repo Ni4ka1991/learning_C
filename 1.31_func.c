dcfc9ecf8cc78a209637b1f21f09eb1bd8bc577a
Veronica <79570212+Ni4ka1991@users.noreply.github.com>
2022-02-11 13:06:56 +0200

    Create 1.31_func.c

---
+++ 1.31_func.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+#define CLS printf("\033c\n")
+#define NLN putchar('\n')
+
+int LionInDesert (int a, int b, int Lion) {
+	int i, x;
+	for (i = 0; i < 11; i++) { 
+		x = (a + b) >> 1;
+		if (Lion == x) {
+			printf("\nБыстро мы словили Льва\n");		
+			break;
+			}
+			else {
+				
+				}
+	}
+
+int main (int n, int lmt) {
+	CLS;
+	LionInDesert (1; 1024; 576);
+	NLN;
+	
+
+}	
